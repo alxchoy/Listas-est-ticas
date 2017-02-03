1fbd390ac13b269fca8bac8a11889b863c560d71
Alumno <alumno@mbp-02.local>
2017-02-02 19:12:07 -0500

    Initial Commit

---
+++ Lista estáticas - clase 5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lista estáticas - clase 5
+//
+//  Created by Alumno on 2/02/17.
+//  Copyright © 2017 area51. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
